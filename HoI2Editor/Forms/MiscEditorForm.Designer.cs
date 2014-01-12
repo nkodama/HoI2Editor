eb3a24817fc43103de8ad87ac4b1dc1ed585643d
nkodama <kodama.naoki@gmail.com>
2014-01-12 18:02:21 +0900

    [整理] Misc編集フォームのレイアウト変更
    Signed-off-by: nkodama <kodama.naoki@gmail.com>

--- HoI2Editor/Forms/MiscEditorForm.Designer.cs
+++ HoI2Editor/Forms/MiscEditorForm.Designer.cs
@@ -31,171 +31,52 @@
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MiscEditorForm));
             this.miscTabControl = new System.Windows.Forms.TabControl();
-            this.economyTabPage1 = new System.Windows.Forms.TabPage();
-            this.economyTabPage2 = new System.Windows.Forms.TabPage();
-            this.economyTabPage3 = new System.Windows.Forms.TabPage();
-            this.intelligenceTabPage = new System.Windows.Forms.TabPage();
-            this.diplomacyTabPage = new System.Windows.Forms.TabPage();
-            this.combatTabPage1 = new System.Windows.Forms.TabPage();
-            this.combatTabPage2 = new System.Windows.Forms.TabPage();
-            this.combatTabPage3 = new System.Windows.Forms.TabPage();
-            this.combatTabPage4 = new System.Windows.Forms.TabPage();
-            this.combatTabPage5 = new System.Windows.Forms.TabPage();
-            this.missionTabPage1 = new System.Windows.Forms.TabPage();
-            this.missionTabPage2 = new System.Windows.Forms.TabPage();
-            this.countryTabPage = new System.Windows.Forms.TabPage();
-            this.researchTabPage = new System.Windows.Forms.TabPage();
-            this.tradeTabPage = new System.Windows.Forms.TabPage();
-            this.aiTabPage = new System.Windows.Forms.TabPage();
-            this.modTabPage = new System.Windows.Forms.TabPage();
-            this.mapTabPage = new System.Windows.Forms.TabPage();
             this.miscToolTip = new System.Windows.Forms.ToolTip(this.components);
-            this.miscTabControl.SuspendLayout();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.reloadButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // miscTabControl
             // 
-            this.miscTabControl.Controls.Add(this.economyTabPage1);
-            this.miscTabControl.Controls.Add(this.economyTabPage2);
-            this.miscTabControl.Controls.Add(this.economyTabPage3);
-            this.miscTabControl.Controls.Add(this.intelligenceTabPage);
-            this.miscTabControl.Controls.Add(this.diplomacyTabPage);
-            this.miscTabControl.Controls.Add(this.combatTabPage1);
-            this.miscTabControl.Controls.Add(this.combatTabPage2);
-            this.miscTabControl.Controls.Add(this.combatTabPage3);
-            this.miscTabControl.Controls.Add(this.combatTabPage4);
-            this.miscTabControl.Controls.Add(this.combatTabPage5);
-            this.miscTabControl.Controls.Add(this.missionTabPage1);
-            this.miscTabControl.Controls.Add(this.missionTabPage2);
-            this.miscTabControl.Controls.Add(this.countryTabPage);
-            this.miscTabControl.Controls.Add(this.researchTabPage);
-            this.miscTabControl.Controls.Add(this.tradeTabPage);
-            this.miscTabControl.Controls.Add(this.aiTabPage);
-            this.miscTabControl.Controls.Add(this.modTabPage);
-            this.miscTabControl.Controls.Add(this.mapTabPage);
             resources.ApplyResources(this.miscTabControl, "miscTabControl");
             this.miscTabControl.Name = "miscTabControl";
             this.miscTabControl.SelectedIndex = 0;
             this.miscTabControl.SelectedIndexChanged += new System.EventHandler(this.OnMiscTabControlSelectedIndexChanged);
             // 
-            // economyTabPage1
+            // closeButton
             // 
-            this.economyTabPage1.BackColor = System.Drawing.SystemColors.Control;
-            resources.ApplyResources(this.economyTabPage1, "economyTabPage1");
-            this.economyTabPage1.Name = "economyTabPage1";
+            resources.ApplyResources(this.closeButton, "closeButton");
+            this.closeButton.Name = "closeButton";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.OnCloseButtonClick);
             // 
-            // economyTabPage2
+            // saveButton
             // 
-            this.economyTabPage2.BackColor = System.Drawing.SystemColors.Control;
-            resources.ApplyResources(this.economyTabPage2, "economyTabPage2");
-            this.economyTabPage2.Name = "economyTabPage2";
+            resources.ApplyResources(this.saveButton, "saveButton");
+            this.saveButton.Name = "saveButton";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.OnSaveButtonClick);
             // 
-            // economyTabPage3
+            // reloadButton
             // 
-            this.economyTabPage3.BackColor = System.Drawing.SystemColors.Control;
-            resources.ApplyResources(this.economyTabPage3, "economyTabPage3");
-            this.economyTabPage3.Name = "economyTabPage3";
-            // 
-            // intelligenceTabPage
-            // 
-            this.intelligenceTabPage.BackColor = System.Drawing.SystemColors.Control;
-            resources.ApplyResources(this.intelligenceTabPage, "intelligenceTabPage");
-            this.intelligenceTabPage.Name = "intelligenceTabPage";
-            // 
-            // diplomacyTabPage
-            // 
-            this.diplomacyTabPage.BackColor = System.Drawing.SystemColors.Control;
-            resources.ApplyResources(this.diplomacyTabPage, "diplomacyTabPage");
-            this.diplomacyTabPage.Name = "diplomacyTabPage";
-            // 
-            // combatTabPage1
-            // 
-            this.combatTabPage1.BackColor = System.Drawing.SystemColors.Control;
-            resources.ApplyResources(this.combatTabPage1, "combatTabPage1");
-            this.combatTabPage1.Name = "combatTabPage1";
-            // 
-            // combatTabPage2
-            // 
-            this.combatTabPage2.BackColor = System.Drawing.SystemColors.Control;
-            resources.ApplyResources(this.combatTabPage2, "combatTabPage2");
-            this.combatTabPage2.Name = "combatTabPage2";
-            // 
-            // combatTabPage3
-            // 
-            this.combatTabPage3.BackColor = System.Drawing.SystemColors.Control;
-            resources.ApplyResources(this.combatTabPage3, "combatTabPage3");
-            this.combatTabPage3.Name = "combatTabPage3";
-            // 
-            // combatTabPage4
-            // 
-            this.combatTabPage4.BackColor = System.Drawing.SystemColors.Control;
-            resources.ApplyResources(this.combatTabPage4, "combatTabPage4");
-            this.combatTabPage4.Name = "combatTabPage4";
-            // 
-            // combatTabPage5
-            // 
-            this.combatTabPage5.BackColor = System.Drawing.SystemColors.Control;
-            resources.ApplyResources(this.combatTabPage5, "combatTabPage5");
-            this.combatTabPage5.Name = "combatTabPage5";
-            // 
-            // missionTabPage1
-            // 
-            this.missionTabPage1.BackColor = System.Drawing.SystemColors.Control;
-            resources.ApplyResources(this.missionTabPage1, "missionTabPage1");
-            this.missionTabPage1.Name = "missionTabPage1";
-            // 
-            // missionTabPage2
-            // 
-            this.missionTabPage2.BackColor = System.Drawing.SystemColors.Control;
-            resources.ApplyResources(this.missionTabPage2, "missionTabPage2");
-            this.missionTabPage2.Name = "missionTabPage2";
-            // 
-            // countryTabPage
-            // 
-            this.countryTabPage.BackColor = System.Drawing.SystemColors.Control;
-            resources.ApplyResources(this.countryTabPage, "countryTabPage");
-            this.countryTabPage.Name = "countryTabPage";
-            // 
-            // researchTabPage
-            // 
-            this.researchTabPage.BackColor = System.Drawing.SystemColors.Control;
-            resources.ApplyResources(this.researchTabPage, "researchTabPage");
-            this.researchTabPage.Name = "researchTabPage";
-            // 
-            // tradeTabPage
-            // 
-            this.tradeTabPage.BackColor = System.Drawing.SystemColors.Control;
-            resources.ApplyResources(this.tradeTabPage, "tradeTabPage");
-            this.tradeTabPage.Name = "tradeTabPage";
-            // 
-            // aiTabPage
-            // 
-            this.aiTabPage.BackColor = System.Drawing.SystemColors.Control;
-            resources.ApplyResources(this.aiTabPage, "aiTabPage");
-            this.aiTabPage.Name = "aiTabPage";
-            // 
-            // modTabPage
-            // 
-            this.modTabPage.BackColor = System.Drawing.SystemColors.Control;
-            resources.ApplyResources(this.modTabPage, "modTabPage");
-            this.modTabPage.Name = "modTabPage";
-            // 
-            // mapTabPage
-            // 
-            this.mapTabPage.BackColor = System.Drawing.SystemColors.Control;
-            resources.ApplyResources(this.mapTabPage, "mapTabPage");
-            this.mapTabPage.Name = "mapTabPage";
+            resources.ApplyResources(this.reloadButton, "reloadButton");
+            this.reloadButton.Name = "reloadButton";
+            this.reloadButton.UseVisualStyleBackColor = true;
+            this.reloadButton.Click += new System.EventHandler(this.OnReloadButtonClick);
             // 
             // MiscEditorForm
             // 
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.reloadButton);
             this.Controls.Add(this.miscTabControl);
             this.Name = "MiscEditorForm";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.OnMiscEditorFormClosing);
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.OnMiscEditorFormClosed);
             this.Load += new System.EventHandler(this.OnMiscEditorFormLoad);
-            this.miscTabControl.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -203,24 +84,9 @@
         #endregion
 
         private System.Windows.Forms.TabControl miscTabControl;
-        private System.Windows.Forms.TabPage economyTabPage1;
-        private System.Windows.Forms.TabPage economyTabPage3;
-        private System.Windows.Forms.TabPage economyTabPage2;
-        private System.Windows.Forms.TabPage intelligenceTabPage;
-        private System.Windows.Forms.TabPage diplomacyTabPage;
-        private System.Windows.Forms.TabPage combatTabPage1;
-        private System.Windows.Forms.TabPage combatTabPage2;
-        private System.Windows.Forms.TabPage combatTabPage3;
-        private System.Windows.Forms.TabPage combatTabPage4;
-        private System.Windows.Forms.TabPage combatTabPage5;
-        private System.Windows.Forms.TabPage missionTabPage1;
-        private System.Windows.Forms.TabPage missionTabPage2;
-        private System.Windows.Forms.TabPage countryTabPage;
-        private System.Windows.Forms.TabPage researchTabPage;
-        private System.Windows.Forms.TabPage tradeTabPage;
-        private System.Windows.Forms.TabPage aiTabPage;
-        private System.Windows.Forms.TabPage modTabPage;
-        private System.Windows.Forms.TabPage mapTabPage;
         private System.Windows.Forms.ToolTip miscToolTip;
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button reloadButton;
     }
 }
