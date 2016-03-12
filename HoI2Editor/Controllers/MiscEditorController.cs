0516398f18a529b338940ab18d043f35522ec23e
nkodama <kodama.naoki@gmail.com>
2016-03-12 10:35:03 +0900

    ファイルの追加忘れ修正

---
+++ HoI2Editor/Controllers/MiscEditorController.cs
@@ -0,0 +1,86 @@
+﻿using HoI2Editor.Forms;
+
+namespace HoI2Editor.Controllers
+{
+    /// <summary>
+    ///     基礎データエディタコントローラ
+    /// </summary>
+    internal class MiscEditorController
+    {
+        #region 内部フィールド
+
+        /// <summary>
+        ///     フォーム
+        /// </summary>
+        private MiscEditorForm _form;
+
+        #endregion
+
+        #region フォーム管理
+
+        /// <summary>
+        ///     フォームを開く
+        /// </summary>
+        internal void OpenForm()
+        {
+            if (_form == null)
+            {
+                _form = new MiscEditorForm(this);
+                _form.Show();
+            }
+            else
+            {
+                _form.Activate();
+            }
+        }
+
+        /// <summary>
+        ///     フォームが存在するかどうかを返す
+        /// </summary>
+        /// <returns>フォームが存在すればtrueを返す</returns>
+        internal bool ExistsForm()
+        {
+            return _form != null;
+        }
+
+        /// <summary>
+        ///     フォームクローズ時の処理
+        /// </summary>
+        internal void OnFormClosed()
+        {
+            _form = null;
+            HoI2EditorController.OnEditorStatusUpdate();
+        }
+
+        #endregion
+
+        #region データ処理
+
+        /// <summary>
+        ///     データ読み込み後の処理
+        /// </summary>
+        internal void OnFileLoaded()
+        {
+            _form?.OnFileLoaded();
+        }
+
+        /// <summary>
+        ///     データ保存後の処理
+        /// </summary>
+        internal void OnFileSaved()
+        {
+            _form?.OnFileSaved();
+        }
+
+        /// <summary>
+        ///     編集項目変更後の処理
+        /// </summary>
+        /// <param name="id">編集項目ID</param>
+        internal void OnItemChanged(EditorItemId id)
+        {
+            _form?.OnItemChanged(id);
+        }
+
+        #endregion
+    }
+}
---
+++ HoI2Editor/Controllers/ModelNameEditorController.cs
@@ -0,0 +1,86 @@
+﻿using HoI2Editor.Forms;
+
+namespace HoI2Editor.Controllers
+{
+    /// <summary>
+    ///     モデル名エディタコントローラ
+    /// </summary>
+    internal class ModelNameEditorController
+    {
+        #region 内部フィールド
+
+        /// <summary>
+        ///     フォーム
+        /// </summary>
+        private ModelNameEditorForm _form;
+
+        #endregion
+
+        #region フォーム管理
+
+        /// <summary>
+        ///     フォームを開く
+        /// </summary>
+        internal void OpenForm()
+        {
+            if (_form == null)
+            {
+                _form = new ModelNameEditorForm(this);
+                _form.Show();
+            }
+            else
+            {
+                _form.Activate();
+            }
+        }
+
+        /// <summary>
+        ///     フォームが存在するかどうかを返す
+        /// </summary>
+        /// <returns>フォームが存在すればtrueを返す</returns>
+        internal bool ExistsForm()
+        {
+            return _form != null;
+        }
+
+        /// <summary>
+        ///     フォームクローズ時の処理
+        /// </summary>
+        internal void OnFormClosed()
+        {
+            _form = null;
+            HoI2EditorController.OnEditorStatusUpdate();
+        }
+
+        #endregion
+
+        #region データ処理
+
+        /// <summary>
+        ///     データ読み込み後の処理
+        /// </summary>
+        internal void OnFileLoaded()
+        {
+            _form?.OnFileLoaded();
+        }
+
+        /// <summary>
+        ///     データ保存後の処理
+        /// </summary>
+        internal void OnFileSaved()
+        {
+            _form?.OnFileSaved();
+        }
+
+        /// <summary>
+        ///     編集項目変更後の処理
+        /// </summary>
+        /// <param name="id">編集項目ID</param>
+        internal void OnItemChanged(EditorItemId id)
+        {
+            _form?.OnItemChanged(id);
+        }
+
+        #endregion
+    }
+}
